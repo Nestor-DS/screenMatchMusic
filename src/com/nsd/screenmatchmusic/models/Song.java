8885391535e64f2d74de781c96367d480db6d884
Nestor-DS <nestorduhamel18@outlook.es>
2024-04-10 00:53:33 -0600

    class Song(getters and setters)

---
+++ src/com/nsd/screenmatchmusic/models/Song.java
@@ -0,0 +1,33 @@
+package com.nsd.screenmatchmusic.models;
+
+public class Song extends Audio{
+    private String artist;
+    private String album;
+    private String genre;
+    // getters and setters
+
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
