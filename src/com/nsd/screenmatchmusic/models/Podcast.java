c6f2d1e94d7960cc2fc922a64eb1805c2b940579
Nestor-DS <nestorduhamel18@outlook.es>
2024-04-10 00:53:06 -0600

    class podcast(getters and setters)

---
+++ src/com/nsd/screenmatchmusic/models/Podcast.java
@@ -0,0 +1,32 @@
+package com.nsd.screenmatchmusic.models;
+
+public class Podcast extends Audio{
+    private String author;
+    private String description;
+    private int totalEpisodes;
+    // getters and setters
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotalEpisodes() {
+        return totalEpisodes;
+    }
+
+    public void setTotalEpisodes(int totalEpisodes) {
+        this.totalEpisodes = totalEpisodes;
+    }
+}
